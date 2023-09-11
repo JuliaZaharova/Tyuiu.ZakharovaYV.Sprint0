221032a18d339c3c3f0802315117ad283bd8f1d0
Tyuiu.ZakharovaYV@outlook.com <justik4433@gmail.com>
2023-09-11 23:56:43 +0500

    Task3

---
+++ Tyuiu.ZakharovaYV.Sprint0.Task3.V0.Lib/Tyuiu.ZakharovaYV.Sprint0.Task3.V0.Lib.cpp
@@ -0,0 +1,18 @@
+// Tyuiu.ZakharovaYV.Sprint0.Task3.V0.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: This is an example of a library function
+class Service1  : public ISprint0Task3
+{
+
+
+	// Inherited via ISprint0Task3V0
+	virtual int Add(int a, int b, int c) override
+	{
+		return a + b + c; // решение 
+	};
+};
